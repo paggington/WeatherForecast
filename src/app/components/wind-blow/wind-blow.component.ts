d987c203e2d089d8abe496ea4a7c5d596fee632e
Artem <lobach.artem.v@gmail.com>
2022-03-06 17:31:01 +0100

    compass code

---
+++ src/app/components/wind-blow/wind-blow.component.ts
@@ -0,0 +1,33 @@
+import {ApplicationRef, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'app-wind-blow',
+  templateUrl: './wind-blow.component.html',
+  styleUrls: ['./wind-blow.component.css']
+})
+export class WindBlowComponent implements OnInit,OnChanges{
+  @Input()
+  degree?:number=0.0;
+  constructor() {
+  }
+  ngOnInit(): void {
+    this.update();
+  }
+  update(){
+    let elem=document.getElementById("windImage");
+    //@ts-ignore
+    elem.style.setProperty('transform','rotate('+this.degree+'deg)');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    for (let propName in changes) {
+      let change = changes[propName];
+
+      let curVal  = JSON.stringify(change.currentValue);
+      let prevVal = JSON.stringify(change.previousValue);
+      if(curVal!=prevVal){
+        this.update();
+      }
+    }
+  }
+
+}
