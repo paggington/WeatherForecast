22f68d1667aaee5f8ab56577c834150fe5ef16d6
Artem <lobach.artem.v@gmail.com>
2022-03-09 15:55:17 +0100

    new model

---
+++ src/app/components/map-c/map-c.component.ts
@@ -0,0 +1,51 @@
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Loader} from "@googlemaps/js-api-loader";
+
+@Component({
+  selector: 'app-map-c',
+  templateUrl: './map-c.component.html',
+  styleUrls: ['./map-c.component.css']
+})
+export class MapCComponent implements OnInit, OnChanges {
+  @Input()
+  longitude?: number | 131.036;
+  @Input()
+  latitude?: number | -25.344;
+
+  constructor() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+        this.initCart();
+    }
+
+  ngOnInit(): void {
+    this.initCart();
+  }
+
+  initCart() {
+    let loader = new Loader({
+      apiKey: 'AIzaSyD0PI5JEPBGR39vETuLhjCi5lnrJS0l14E',
+      region:'PL',
+      version: 'weekly'
+    })
+    loader.load().then(() => {
+      const point = {lat: this.latitude, lng: this.longitude} as google.maps.MapOptions;
+
+      // @ts-ignore
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          zoom: 6,// @ts-ignore
+          center: point
+        }
+      )
+
+      // @ts-ignore
+      const marker = new google.maps.Marker({// @ts-ignore
+        position: point,
+        map: map
+      })
+    })
+  }
+}
